96849d89ef79228295af7343b72cdaaec9df3fa9
chen <986311827@qq.com>
2026-09-02 21:40:20 +0800

    chore: bump app version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.chanooh.alert"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildFeatures {
